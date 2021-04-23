81880b66dbf1e29dff0a24edd266b6024a5f4816
AKliz <lizotcweb@gmail.com>
2021-04-22 19:47:13 -0800

    lesson3 activity & start of assignment

--- lesson03/activity/dogs.js
+++ lesson03/activity/dogs.js
@@ -1,6 +1,7 @@
-// Create the Dog class constructor
-let barkLevel = 10;
-let energyLevel = 0;
+// Create the Dog class constructor OPTION 1
+
+let barkLevel = 0;
+let energyLevel =10;
 
 class dog{
   constructor(name, age, breed, color, barkLevel, energyLevel){
@@ -18,8 +19,41 @@ class dog{
     console.log(barkLevel , energyLevel);
   }
 }
+
 // Create Marty
 const Marty = new dog('Marty', 4, 'lab', 'chocolate', barkLevel, energyLevel);
+
+// Pat him 5 times
+
+
+Marty.pat();
+Marty.pat();
+Marty.pat();
+Marty.pat();
+Marty.pat();
+
+
+
+//OPTION 2
+//Creat the Dog function using Constructors
+function Dog(name, age, breed, color){
+  this.name = name;
+  this.age = age;
+  this.breed = breed;
+  this.color = color;
+  this.energyLevel = 0;
+  this.barkLevel = 10;
+  this.pat = function(){
+    this.energyLevel++
+    this.barkLevel -= 1
+    console.log(`This dog is called ${this.name} and is ${this.age * 7} human years old. Bark Level is ${this.barkLevel}, energy level is${this.energyLevel}.`)
+  };
+}
+
+//create marty
+
+const marty = new dog('Marty', 3, 'lab', 'chocolate')
+
 // Pat him 5 times
 
 console.log(Marty);
